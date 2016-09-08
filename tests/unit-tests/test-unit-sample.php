75d08c87e5b7f06774388026070e9943dde899a0
Your Name <you@example.com>
2016-09-08 11:43:28 +0200

    ..

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -6,6 +6,12 @@ use phpmock\phpunit\PHPMock;
 class UnitTests extends \PHPUnit_Framework_TestCase{
     use PHPMock;
 
+public function test_wct_get_option()
+
+{
+$this->assertEquals('river-club',wcct_get_option('venue'));
+
+}
 	/**
 	 * A single example test.
 	 */
